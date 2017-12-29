4249fa2d0e6afdf7900222f163585f2e5a91b0ec
Michael Quint <mjq8@cornell.edu>
2017-12-29 16:21:10 -0500

    testing new db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171229180938) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "emails", force: :cascade do |t|
     t.string "to", null: false
     t.string "body", null: false
