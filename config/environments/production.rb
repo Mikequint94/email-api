c5bb92fe06b61c98eeec34fe725490b45bfc3e58
Michael Quint <mjq8@cornell.edu>
2017-12-29 15:26:58 -0500

    added email settings to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "email-api_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'webapisender@gmail.com'}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'webapisender',
+    password: 'ILoveCoding',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
