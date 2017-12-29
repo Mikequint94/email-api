0258998cb107e8f4bf95a3713932595168092c8b
Michael Quint <mjq8@cornell.edu>
2017-12-29 14:55:39 -0500

    setup new email address to act as sender and configured settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,6 @@ class UserMailer < ApplicationMailer
   
   default from: "mjq8@cornell.edu"
   
-  # layout "mailer"
-  
   def send_email(email)
     @email = email
     mail(to: @email.to, subject: @email.subject)
